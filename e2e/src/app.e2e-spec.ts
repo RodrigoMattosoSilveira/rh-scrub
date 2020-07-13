4da033fce7ec41b953abd9ebc476756f5e035617
Rodrigo Silveira <r3850365@gmail.com>
2020-07-13 15:37:37 -0700

    navigate-to-RH-Site-and-sign-up  -  Got to the Rolling Hills page

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -2,22 +2,58 @@ import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
 
 describe('workspace-project App', () => {
-  let page: AppPage;
-
-  beforeEach(() => {
-    page = new AppPage();
-  });
-
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('rh-scrub app is running!');
-  });
-
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
-  });
+	let page: AppPage;
+	
+	beforeEach(() => {
+		page = new AppPage();
+	});
+	
+	describe('workspace-project App', () => {
+		
+		it('should display welcome message', () => {
+			page.navigateTo();
+			expect(page.getTitleText()).toEqual('rh-scrub app is running!');
+		});
+		
+		afterEach(async () => {
+			// Assert that there are no errors emitted from the browser
+			const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+			expect(logs).not.toContain(jasmine.objectContaining({
+				level: logging.Level.SEVERE,
+			} as logging.Entry));
+		});
+	});
+	
+	describe('Rolling Hills website', () => {
+		describe('is there and have the MEMBER LOGIN button', () => {
+			beforeAll(() => {
+				browser.waitForAngularEnabled(false);
+				page.navigateToRh();
+			});
+			it('should find RH Contact Us', () => {
+				expect(page.getContactUsText()).toEqual('CONTACT US');
+			});
+			it('should find MEMBER LOGIN button', () => {
+				expect(page.getMemberLogInButton()).toEqual('MEMBER LOGIN');
+			});
+			// xdescribe('log in', () => {
+			// 	beforeAll(() => {
+			// 		page.navigateToRhLogin();
+			// 	});
+			// 	it('should find RH Contact Us', () => {
+			// 		expect(page.getContactUsText()).toEqual('CONTACT US');
+			// 	});
+			// 	it('should find MEMBER LOGIN button', () => {
+			// 		expect(page.getMemberLogInButton()).toEqual('MEMBER LOGIN');
+			// 	});
+			//
+			// 	afterAll(async () => {
+			// 		browser.waitForAngularEnabled(true);		});
+			// });
+			afterAll(async () => {
+				browser.waitForAngularEnabled(true);
+			});
+			
+		});
+	});
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,41 @@
-import { browser, by, element } from 'protractor';
+import {browser, by, element, protractor} from 'protractor';
 
 export class AppPage {
-  navigateTo(): Promise<unknown> {
-    return browser.get(browser.baseUrl) as Promise<unknown>;
-  }
+  	navigateTo = (): Promise<unknown> => {
+		return browser.get(browser.baseUrl) as Promise<unknown>;
+	}
 
-  getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
-  }
+	getTitleText = (): Promise<string> => {
+		return element(by.css('app-root .content span')).getText() as Promise<string>;
+	}
+	
+	navigateToRh = (): Promise<unknown> => {
+		return  browser.get('https://www.rollinghillsclub.com/') as Promise<unknown>;
+	}
+	
+	getContactUsText = (): Promise<string> => {
+		return element(by.xpath('/html[1]/body[1]/div[1]/footer[1]/div[1]/div[1]/div[1]/div[1]/div[4]/div[1]/div[1]')).getText() as Promise<string>
+	}
+	
+	getMemberLogInButton = (): Promise<string> => {
+		return Promise.resolve(element(by.xpath("/html[1]/body[1]/div[1]/header[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/h4[1]/a[1]")).getText());
+	}
+	//
+	// navigateToRhLogin = (): Promise<string> => {
+  	// 	// click on the MEMBER LOGIN button
+ 	// 	return element(by.xpath("/html[1]/body[1]/div[1]/header[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/h4[1]/a[1]")).click()
+	// 		.then((el: any) => {
+	// 			element(by.cssSelector("body")).sendKeys(protractor.Key.ENTER, protractor.Key.PAGE_DOWN).then((v2: any) => {
+	// 				return element(by.xpath('/html[1]/body[1]/div[3]/div[5]/div[1]/div[1]/div[2]/div[1]/div[1]/h1[1]/span[1]')).getText() as Promise<string>;
+	// 			});
+	// 		})
+	// }
+	//
+	// navigateToRhLoginPage = (): Promise<unknown> => {
+ 	// 	// find the MEMBER LOGIN button
+	// 	// Click on the MEMBER LOGIN button
+	// 	// Navivate to the next tab
+	// 	// Ensure we have the 'Rolling Hills Club - Online Member & Guest Services' page
+	//
+	// }
 }
